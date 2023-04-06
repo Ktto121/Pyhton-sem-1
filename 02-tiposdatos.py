e534db6fcbfa974b710d8bf7c850e1fedcb62937
Miguel <Ktto121@users.noreply.github.com>
2023-04-06 16:02:40 -0400

    Tipo de datos

---
+++ 02-tiposdatos.py
@@ -0,0 +1,28 @@
+#01 datos de tipo númerico
+estatura = 1.65
+peso = 68
+complejo = 1 +4j
+print ("Impresion del numero completjo:", complejo)
+
+#operacion arimetica basica
+inc= peso/estatura**2
+print("Mi INC es de:",inc)
+
+#02 Datos de tipo cadena de caracteres
+asignatura = "Programacion"
+carrera = "Ingieneria civil en informatica"
+
+print ("Mi carrera es", str(carrera), "Y me encuentro en la asignatura de", str(asignatura))
+
+#03 valores de booleanos
+ampolleta = False
+interruptor = False
+
+#con type sabemos el tipo de datos que estamos tratando
+print(type(ampolleta))
+
+#04 Datos tipo array (objeto de tipo colección)
+estudiante = ["Jose", "Alfonso", "Diego", "Joaquin"]
+num = [1,2,3,4,5,6]
+print(estudiante) 
+print(num)
