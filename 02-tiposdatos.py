b5cd55d46b2e23474f91991050ca5aea8af84522
Miguel <Ktto121@users.noreply.github.com>
2023-04-11 15:49:35 -0400

    Clase numero 2

--- 02-tiposdatos.py
+++ 02-tiposdatos.py
@@ -32,6 +32,7 @@ estudiante = ["Jose", "Alfonso", "Diego", "Joaquin"]
 num = [1,2,3,4,5,6]
 print(estudiante) 
 print(num)
+
 #delcarando e inicializando una lista
 nueva_lista = list()
 print("Esta es una lista vacia:",nueva_lista)
@@ -45,3 +46,21 @@ print("Esta es la lista de lo que debemos comprar en el super", compra_de_super)
 print("En total son",len(compra_de_super),"\n""productos por comprar")
 
 print(compra_de_super.count("Papa"))
+
+lenguaje = ("JacaScript")
+print("Nuevo valor del Arreglo de un elemnto:", lenguaje)
+
+#¿Como accedo a un elemento especifico de la lista?
+print(estudiante[0])
+print(estudiante[1])
+print(estudiante[2])
+print(estudiante[-3])
+#Los numeros negativo hace que la lista sea del final hacia el inicio, Pero solo en Pyhton
+
+#Inicializando  otra lista de datos mistos 
+data_asig = [10023,"Programación",1(True)]
+
+ 
+ 
+ #¿Se pueden sumar listas?
+ print("Suma de listas",estudiante[3], num)
