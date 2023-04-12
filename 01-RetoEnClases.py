6eb17addf2c085e55a9f25406f0f47e3fe3c9667
Miguel <Ktto121@users.noreply.github.com>
2023-04-12 17:56:35 -0400

    clase 4

---
+++ 01-RetoEnClases.py
@@ -0,0 +1,5 @@
+nombre = input ("¿Cual es tu nombre?\n")
+edad = input ("¿Cual es tu edad?\n")
+print(type(edad))
+total_edad = int(edad) + 20
+print("Hola mi nombre es", nombre,"y tengo", edad,"años y en 20 años tendre", total_edad)
